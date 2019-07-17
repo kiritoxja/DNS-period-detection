cda949e57f0c27e0d5ca2ca2ca2c386bd34786c0
Elizabeth <41627244+Elizabeth1214akane@users.noreply.github.com>
2019-07-17 11:13:28 +0800

    边和周期类的成员变量

---
+++ coreClass/util.py
@@ -0,0 +1,15 @@
+
+class Edge:
+    pattern = ''   # 后缀树边上的字符串
+    current = []   # 出现向量
+    value = 0      # 后缀树边对应的值
+
+
+class Period:
+    pattern = '' # 周期字符
+    val = 0      # 周期长度
+    stops = 0    # 当前所指的pattern的起始位置
+    length = 0   # pattern的长度
+    foundPosCount = -1 # 传说中的count,pattern在整个字符串中出现的字数
+    conf = 0     # 周期性置信度
+    avgVal = 0   # pattern 的平均周期
